c28421679880244145621a0f8c7187d0dec7c5fd
zewenn <takaczsoli@gmail.com>
2026-04-17 12:39:14 +0200

    add: task 2 - drop_test, using one dimensional array to cover drops

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,9 +1,24 @@
 from pathlib import Path
 
 
+def min_num_of_drops(N, K):
+    backing_array = [0] * (N + 1)
+
+    while backing_array[N] < K:
+        for i in range(N, 0, -1):
+            backing_array[i] += 1 + backing_array[i - 1]
+
+    return backing_array[1]
+
+
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+
+    for line in data.splitlines():
+        line_data = line.split(", ")
+        n = int(line_data[0])
+        k = int(line_data[1])
+        print(min_num_of_drops(n, k))
 
 
 if __name__ == "__main__":
